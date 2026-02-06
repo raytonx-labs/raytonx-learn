13af014891f1cb21d310ee6d38c666e821608cd1
raytonx <contact@raytonx.com>
2026-02-06 11:50:31 +0800

    refactor: unify Supabase client typing and centralize course queries (#6)
    * feat: use TypedSupabaseClient instead of SupabaseClient<Database>
    
    * feat: introduce baseQuery to centralize Supabase query constraints
    
    ---------
    
    Co-authored-by: xiaoteng <xiaoteng@raytonx.com>

---
+++ services/courses/detail.ts
@@ -0,0 +1,23 @@
+import { TypedSupabaseClient } from "@/types/supabase-client";
+
+import { publishedCoursesQuery } from "./queries";
+
+export const getCourseById = async (supabase: TypedSupabaseClient, courseId: string) => {
+  const { data, error } = await publishedCoursesQuery(supabase).eq("id", courseId).single();
+
+  if (error) {
+    throw new Error(`Error fetching course with ID ${courseId}: ${error.message}`);
+  }
+
+  return data;
+};
+
+export const getCourseBySlug = async (supabase: TypedSupabaseClient, courseSlug: string) => {
+  const { data, error } = await publishedCoursesQuery(supabase).eq("slug", courseSlug).single();
+
+  if (error) {
+    throw new Error(`Error fetching course with ID ${courseSlug}: ${error.message}`);
+  }
+
+  return data;
+};
