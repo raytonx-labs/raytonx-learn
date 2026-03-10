9eab0c1ac3b640329c2339b0cc01e724611b710b
xiaoteng <xiaoteng@raytonx.com>
2026-03-10 18:01:03 +0800

    feat: set max width for lesson page layout

--- app/courses/[courseSlug]/lessons/[lessonSlug]/layout.tsx
+++ app/courses/[courseSlug]/lessons/[lessonSlug]/layout.tsx
@@ -1,3 +1,3 @@
 export default function LessonLayout({ children }: { children: React.ReactNode }) {
-  return <div className="flex h-full">{children}</div>;
+  return <div className="flex h-full max-w-6xl mx-auto px-4 py-8">{children}</div>;
 }
