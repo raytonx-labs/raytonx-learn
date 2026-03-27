6351ef831e467385181bae06f999af85657a06cf
xiaoteng <xiaoteng@raytonx.com>
2026-03-27 20:21:14 +0800

    fix: disable trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/courses",
+  trailingSlash: false,
 };
 
 export default nextConfig;
