7da19c927f9405f21f136ab7488e8c947f191fb4
xiaoteng <xiaoteng@raytonx.com>
2026-03-28 18:56:11 +0800

    fix: revalidatePath page

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
 
   revalidatePath(`/courses/${courseSlug}`, "layout");
   // 重新更新课时内容，实现SSG更新
-  revalidatePath(`/courses/${courseSlug}/lessons/${lessonSlug}`);
+  revalidatePath(`/courses/${courseSlug}/lessons/${lessonSlug}`, "page");
 
   return Response.json({ message: "Static site updated" });
 }
