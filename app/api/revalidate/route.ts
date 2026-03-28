1e847ac3ea2fa87dc3bcab802ad923887f719c90
xiaoteng <xiaoteng@raytonx.com>
2026-03-28 18:24:59 +0800

    feat: add revalidate api

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,15 @@
+import { revalidatePath } from "next/cache";
+
+export async function POST(request: Request) {
+  const { courseSlug, lessonSlug, secret } = await request.json();
+
+  if (secret !== process.env.REVALIDATE_SECRET) {
+    return Response.json({ message: "Invalid secret" }, { status: 401 });
+  }
+
+  revalidatePath(`/course/${courseSlug}`, "layout");
+  // 重新更新课时内容，实现SSG更新
+  revalidatePath(`/course/${courseSlug}/lesson/${lessonSlug}`);
+
+  return Response.json({ message: "Static site updated" });
+}
